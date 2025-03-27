f30b1c70f2cd0f4f34d18e58080d4d1e98a39bd1
Alexander <89583501+Leo4556@users.noreply.github.com>
2025-03-27 21:50:40 +0300

    Добавлен дизайн страницы Настройки

--- app/src/main/java/com/example/recipeguide/OptionsScreen.java
+++ app/src/main/java/com/example/recipeguide/OptionsScreen.java
@@ -1,51 +1,46 @@
 package com.example.recipeguide;
 
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
 
 public class OptionsScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_options_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
+        setContentView(R.layout.activity_options_screen);}
 
 
 
-    public void goHome(View view){
+    public void goHome(View view) {
         Intent intent = new Intent(this, MainScreen.class);
         startActivity(intent);
     }
 
-    public void goAddScreen(View view){
+    public void goAddScreen(View view) {
         Intent intent = new Intent(this, AddScreen.class);
         startActivity(intent);
     }
 
-    public void goFavourites(View view){
+    public void goFavourites(View view) {
         Intent intent = new Intent(this, FavouritesScreen.class);
         startActivity(intent);
     }
 
-    public void goOptions(View view){
+    public void goOptions(View view) {
         Intent intent = new Intent(this, OptionsScreen.class);
         startActivity(intent);
     }
-
-
-
 }
