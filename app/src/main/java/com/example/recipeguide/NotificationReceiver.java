ab2f82e1c0f58c09db10bbbd5656fa3f8ab2a295
Alexander <89583501+Leo4556@users.noreply.github.com>
2025-04-04 19:24:20 +0300

    Получение уведомлений зависят от переключателя в OptionsScreen

--- app/src/main/java/com/example/recipeguide/NotificationReceiver.java
+++ app/src/main/java/com/example/recipeguide/NotificationReceiver.java
@@ -3,10 +3,16 @@ package com.example.recipeguide;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 public class NotificationReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationHelper.showNotification(context, "Напоминание!", "Не забудьте попробовать новый рецепт сегодня!");
+        SharedPreferences preferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+        boolean notificationsEnabled = preferences.getBoolean("notifications_enabled", false);
+
+        if (notificationsEnabled) {
+            NotificationHelper.showNotification(context, "Напоминание!", "Не забудьте попробовать новый рецепт сегодня!");
+        }
     }
 }
